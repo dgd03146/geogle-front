d998cd61b1c9e30a4447bf3d705f6cd5257542f5
Marekgr7 <Marekgr7@gmail.com>
2021-07-11 07:43:54 +0200

    comments added

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,51 +1,57 @@
-import React, { useEffect } from 'react';
-import logo from '../resources/logo.png';
-import ActiveUsersList from './components/ActiveUsersList/ActiveUsersList';
-import * as webRTCHandler from '../utils/webRTC/webRTCHandler';
-import * as webRTCGroupHandler from '../utils/webRTC/webRTCGroupCallHandler';
-import DirectCall from './components/DirectCall/DirectCall';
-import { connect } from 'react-redux';
-import DashboardInformation from './components/DashboardInformation/DashboardInformation';
-import { callStates } from '../store/actions/callActions';
-import GroupCallRoomsList from './components/GroupCallRoomsList/GroupCallRoomsList';
-import GroupCall from './components/GroupCall/GroupCall';
-import axios from 'axios';
-import { setTurnServers } from '../utils/webRTC/TURN';
+import React, { useEffect } from "react";
+import logo from "../resources/logo.png";
+import ActiveUsersList from "./components/ActiveUsersList/ActiveUsersList";
+import * as webRTCHandler from "../utils/webRTC/webRTCHandler";
+import * as webRTCGroupHandler from "../utils/webRTC/webRTCGroupCallHandler";
+import DirectCall from "./components/DirectCall/DirectCall";
+import { connect } from "react-redux";
+import DashboardInformation from "./components/DashboardInformation/DashboardInformation";
+import { callStates } from "../store/actions/callActions";
+import GroupCallRoomsList from "./components/GroupCallRoomsList/GroupCallRoomsList";
+import GroupCall from "./components/GroupCall/GroupCall";
+import axios from "axios";
+import { setTurnServers } from "../utils/webRTC/TURN";
 
-import './Dashboard.css';
+import "./Dashboard.css";
 
 const Dashboard = ({ username, callState }) => {
   useEffect(() => {
-    axios.get('https://video-talker-backend-udemy.herokuapp.com/api/get-turn-credentials').then(
-      responseData => {
+    // HERE PLACE YOUR BACKEND SERVER ADDRESS TO FETCH TURN CREDENTIALS
+    axios
+      .get(
+        "https://video-talker-backend-udemy.herokuapp.com/api/get-turn-credentials"
+      )
+      .then((responseData) => {
         console.log(responseData);
         setTurnServers(responseData.data.token.iceServers);
         webRTCHandler.getLocalStream();
         webRTCGroupHandler.connectWithMyPeer();
-      }
-    ). catch(err => {
-      console.log(err);
-    })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
-    <div className='dashboard_container background_main_color'>
-      <div className='dashboard_left_section'>
-        <div className='dashboard_content_container'>
+    <div className="dashboard_container background_main_color">
+      <div className="dashboard_left_section">
+        <div className="dashboard_content_container">
           <DirectCall />
           <GroupCall />
-          {callState !== callStates.CALL_IN_PROGRESS && <DashboardInformation username={username} />}
+          {callState !== callStates.CALL_IN_PROGRESS && (
+            <DashboardInformation username={username} />
+          )}
         </div>
-        <div className='dashboard_rooms_container background_secondary_color'>
+        <div className="dashboard_rooms_container background_secondary_color">
           <GroupCallRoomsList />
         </div>
       </div>
-      <div className='dashboard_right_section background_secondary_color'>
-        <div className='dashboard_active_users_list'>
+      <div className="dashboard_right_section background_secondary_color">
+        <div className="dashboard_active_users_list">
           <ActiveUsersList />
         </div>
-        <div className='dashboard_logo_container'>
-          <img className='dashboard_logo_image' src={logo} />
+        <div className="dashboard_logo_container">
+          <img className="dashboard_logo_image" src={logo} />
         </div>
       </div>
     </div>
@@ -54,7 +60,7 @@ const Dashboard = ({ username, callState }) => {
 
 const mapStateToProps = ({ call, dashboard }) => ({
   ...call,
-  ...dashboard
+  ...dashboard,
 });
 
 export default connect(mapStateToProps)(Dashboard);
