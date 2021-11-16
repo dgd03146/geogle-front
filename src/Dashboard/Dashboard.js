e5324102b82c90140a194e192611329102595bae
jung <ibory1220@naver.com>
2021-11-17 01:39:22 +0900

    complete video talker front end

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,33 +1,31 @@
-import React, { useEffect } from "react";
-import logo from "../resources/logo.png";
-import ActiveUsersList from "./components/ActiveUsersList/ActiveUsersList";
-import * as webRTCHandler from "../utils/webRTC/webRTCHandler";
-import * as webRTCGroupHandler from "../utils/webRTC/webRTCGroupCallHandler";
-import DirectCall from "./components/DirectCall/DirectCall";
-import { connect } from "react-redux";
-import DashboardInformation from "./components/DashboardInformation/DashboardInformation";
-import { callStates } from "../store/actions/callActions";
-import GroupCallRoomsList from "./components/GroupCallRoomsList/GroupCallRoomsList";
-import GroupCall from "./components/GroupCall/GroupCall";
-import axios from "axios";
-import { setTurnServers } from "../utils/webRTC/TURN";
+import React, { useEffect } from 'react';
+import logo from '../resources/logo.png';
+import ActiveUsersList from './components/ActiveUsersList/ActiveUsersList';
+import * as webRTCHandler from '../utils/webRTC/webRTCHandler';
+import * as webRTCGroupHandler from '../utils/webRTC/webRTCGroupCallHandler';
+import DirectCall from './components/DirectCall/DirectCall';
+import { connect } from 'react-redux';
+import DashboardInformation from './components/DashboardInformation/DashboardInformation';
+import { callStates } from '../store/actions/callActions';
+import GroupCallRoomsList from './components/GroupCallRoomsList/GroupCallRoomsList';
+import GroupCall from './components/GroupCall/GroupCall';
+import axios from 'axios';
+import { setTurnServers } from '../utils/webRTC/TURN';
 
-import "./Dashboard.css";
+import './Dashboard.css';
 
 const Dashboard = ({ username, callState }) => {
   useEffect(() => {
     // HERE PLACE YOUR BACKEND SERVER ADDRESS TO FETCH TURN CREDENTIALS
     axios
-      .get(
-        "https://video-talker-backend-udemy.herokuapp.com/api/get-turn-credentials"
-      )
-      .then((responseData) => {
+      .get('https://geogle-backend.herokuapp.com/api/get-turn-credentials')
+      .then(responseData => {
         console.log(responseData);
         setTurnServers(responseData.data.token.iceServers);
         webRTCHandler.getLocalStream();
         webRTCGroupHandler.connectWithMyPeer();
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }, []);
